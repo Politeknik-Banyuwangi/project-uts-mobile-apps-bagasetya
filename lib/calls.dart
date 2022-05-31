8aa6801182c2c6e2fc12576b95a01a3d5d932e40
FlutLab.io <>
2022-05-31 07:14:35 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -5,6 +5,20 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class Calls extends StatelessWidget {
   const Calls({Key? key}) : super(key: key);
   // Masukkan coding disini
+  Widget getCalls(title, subTitle) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage("https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg"),
+        radius: 30,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+      ),
+      subtitle: Text(subTitle),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +32,7 @@ class Calls extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-
+            getCalls("Sasuke", "Today 08.13"),
             // Masukkan Coding disini jangan rubah lainnya
             //
             //
