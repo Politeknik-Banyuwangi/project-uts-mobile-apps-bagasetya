680314afc47fefee742ff697f645e7b3a78a6a5b
FlutLab.io <>
2022-05-31 06:49:20 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -1,22 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Calls extends StatelessWidget {
-  final String? chatMessage;
-  final String? chatTitle;
-  final String? callStatus;
-  final String? imageUrl;
-  final String? callType;
-
-  const Calls({
-    Key? key,
-    this.chatMessage,
-    this.chatTitle,
-    this.callStatus,
-    this.imageUrl,
-    this.callType,
-  }) : super(key: key);
+  const Calls({Key? key}) : super(key: key);
   // Masukkan coding disini
-  //
+  Widget getCalls(
+    title,
+    subTitle,
+  ) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage("https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg"),
+        radius: 30,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+      ),
+      subtitle: Text(subTitle),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,35 +31,18 @@ class Calls extends StatelessWidget {
           return true;
         },
         child: ListView(
-          children: const [
-            Calls(
-              callStatus: 'Outgoing',
-              callType: 'Audio',
-              chatMessage: 'Today, 12:28 PM',
-              chatTitle: 'Arya Stark',
-              imageUrl: 'https://static-koimoi.akamaized.net/wp-content/new-galleries/2020/09/maisie-williams-aka-arya-stark-of-game-of-thrones-someone-told-me-in-season-three-that-i-was-going-to-kill-the-night-king001.jpg',
+          children: [
+            SizedBox(
+              height: 10,
             ),
-            Calls(
-              callStatus: 'Incoming',
-              callType: 'Video',
-              chatMessage: 'Today, 01:11 AM',
-              chatTitle: 'Cersei Lannister',
-              imageUrl: 'https://pyxis.nymag.com/v1/imgs/8a2/6a6/8ddcaf4454e34b72484c5393560af17366-09-cersei-lannister.rsquare.w700.jpg',
-            ),
-            Calls(
-              callStatus: 'Incoming',
-              callType: 'Video',
-              chatMessage: 'Today, 5:28 AM',
-              chatTitle: 'Red Woman',
-              imageUrl: 'https://upload.wikimedia.org/wikipedia/en/8/80/Melisandre-Carice_van_Houten.jpg',
-            ),
-            Calls(
-              callStatus: 'Outgoing',
-              callType: 'Audio',
-              chatMessage: 'Today, 12:28 PM',
-              chatTitle: 'The Mountain',
-              imageUrl: '/data/FLUTTER REVIVAL/whatsapp_ui/lib/assets/images/Mountains.jpg',
-            ),
+            getCalls("Sasuke", "Today at 19.55"),
+            // Masukkan Coding disini jangan rubah lainnya
+            //
+            //
+            //
+            //
+            //
+            //
           ],
         ),
       ),
