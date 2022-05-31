b1bfa340613e67af5178235bbec29c568fce56cb
FlutLab.io <>
2022-05-31 05:15:24 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -28,18 +28,35 @@ class Calls extends StatelessWidget {
           return true;
         },
         child: ListView(
-          children: [
-            SizedBox(
-              height: 10,
+          children: const [
+            Calls(
+              callStatus: 'Outgoing',
+              callType: 'Audio',
+              chatMessage: 'Today, 12:28 PM',
+              chatTitle: 'Arya Stark',
+              imageUrl: 'https://static-koimoi.akamaized.net/wp-content/new-galleries/2020/09/maisie-williams-aka-arya-stark-of-game-of-thrones-someone-told-me-in-season-three-that-i-was-going-to-kill-the-night-king001.jpg',
             ),
-
-            // Masukkan Coding disini jangan rubah lainnya
-            //
-            //
-            //
-            //
-            //
-            //
+            Calls(
+              callStatus: 'Incoming',
+              callType: 'Video',
+              chatMessage: 'Today, 01:11 AM',
+              chatTitle: 'Cersei Lannister',
+              imageUrl: 'https://pyxis.nymag.com/v1/imgs/8a2/6a6/8ddcaf4454e34b72484c5393560af17366-09-cersei-lannister.rsquare.w700.jpg',
+            ),
+            Calls(
+              callStatus: 'Incoming',
+              callType: 'Video',
+              chatMessage: 'Today, 5:28 AM',
+              chatTitle: 'Red Woman',
+              imageUrl: 'https://upload.wikimedia.org/wikipedia/en/8/80/Melisandre-Carice_van_Houten.jpg',
+            ),
+            Calls(
+              callStatus: 'Outgoing',
+              callType: 'Audio',
+              chatMessage: 'Today, 12:28 PM',
+              chatTitle: 'The Mountain',
+              imageUrl: '/data/FLUTTER REVIVAL/whatsapp_ui/lib/assets/images/Mountains.jpg',
+            ),
           ],
         ),
       ),
